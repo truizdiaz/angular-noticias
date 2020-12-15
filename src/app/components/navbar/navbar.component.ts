21f1544de3af28bbd6c41c1e6e21b0d942727782
truizdiaz <tomiruizdiaz@gmail.com>
2020-12-15 11:12:08 -0300

    NavbarComponent

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  @Input() titulo: string;
 
-  constructor() { }
+  constructor() {
+    this.titulo = '';
+   }
 
   ngOnInit(): void {
   }
