dbb72c47cbd31af5ce5ecabf53e28ed138b7cd74
truizdiaz <tomiruizdiaz@gmail.com>
2020-12-15 12:08:01 -0300

    Formulario bind datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+// Componentes
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './components/formulario/formulario.component';
 import { ListadoNoticiasComponent } from './components/listado-noticias/listado-noticias.component';
@@ -16,7 +18,8 @@ import { SpinnerComponent } from './shared/spinner/spinner.component';
     SpinnerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
+  categoriaSeleccionada = 'general';
+  paisSeleccionado = 'ar';
+
+  categorias: any[] = [
+    { value: 'general', nombre: 'General'},
+    { value: 'business', nombre: 'Negocios'},
+    { value: 'entertainment', nombre: 'Entretenimiento'},
+    { value: 'health', nombre: 'Salud'},
+    { value: 'science', nombre: 'Ciencia'},
+    { value: 'sports', nombre: 'Deportes'},
+    { value: 'technology', nombre: 'Tecnología'},
+]
+
+paises: any[] = [
+  { value: 'ar', nombre: 'Argentina'},
+  { value: 'br', nombre: 'Brasil'},
+  { value: 'fr', nombre: 'Francia'},
+  { value: 'hu', nombre: 'Hungria'},
+  { value: 'mx', nombre: 'Mexico'},
+  { value: 'gb', nombre: 'Reino Unido'},
+]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  buscarNoticia() {
+    console.log(this.categoriaSeleccionada);
+    console.log(this.paisSeleccionado);
   }
 
 }
